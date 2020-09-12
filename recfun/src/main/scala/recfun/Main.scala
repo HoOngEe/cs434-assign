7c37ec7f842ac28631087fec6d145f8fd1561fcd
Seonpyo Kim <tomandjerry@postech.ac.kr>
2020-09-12 18:43:31 +0900

    Substitute precondition comments to Scala requires
    I newly learned how to enforce preconditions of passed parameters
    in a function

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,11 +14,12 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int =
-  // Invariant: c, r >= 0
+  def pascal(c: Int, r: Int): Int = {
+    require(c >= 0 && r >= 0)
     if (c == 0) 1
     else if (c > r) 0
     else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
 
   /**
    * Exercise 2
@@ -37,12 +38,13 @@ object Main {
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int =
-  // Invariant: money >= 0
+  def countChange(money: Int, coins: List[Int]): Int = {
+    require(money >= 0)
     if (money == 0) 1
     else {
       if (coins.isEmpty) 0
       else if (coins.head > money) countChange(money, coins.tail)
       else countChange(money - coins.head, coins) + countChange(money, coins.tail)
     }
+  }
 }
